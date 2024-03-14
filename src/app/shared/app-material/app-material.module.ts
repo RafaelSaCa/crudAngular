2b534a96d91c3a0ad2aa942b04e02abb1f88e5ec
Rafael Saca <raffaellsaca@gmail.com>
2024-03-14 20:33:28 -0300

    feat: Estilização toolbar

--- src/app/shared/app-material/app-material.module.ts
+++ src/app/shared/app-material/app-material.module.ts
@@ -3,7 +3,9 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatTableModule } from '@angular/material/table';
 import {MatPaginatorModule} from '@angular/material/paginator';
 import {MatIconModule} from '@angular/material/icon';
-
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
 
 
 @NgModule({
@@ -11,7 +13,10 @@ import {MatIconModule} from '@angular/material/icon';
       MatTableModule,
       MatFormFieldModule,
       MatPaginatorModule,
-      MatIconModule
+      MatIconModule,
+      MatCardModule,
+      MatButtonModule,
+      MatInputModule
 
     ],
 })
