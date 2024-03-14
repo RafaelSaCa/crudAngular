5224191d17ea8e31c0e0822af5932ec9144e9af1
Rafael Saca <raffaellsaca@gmail.com>
2024-03-14 20:28:15 -0300

    feat: Listagem de funcionarios

--- src/app/funcionarios/lista-funcionarios/lista-funcionarios.component.ts
+++ src/app/funcionarios/lista-funcionarios/lista-funcionarios.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Funcionario } from './../model/funcionario';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-lista-funcionarios',
   templateUrl: './lista-funcionarios.component.html',
   styleUrls: ['./lista-funcionarios.component.scss']
 })
-export class ListaFuncionariosComponent {
+export class ListaFuncionariosComponent implements OnInit {
 
+  funcionarios: Funcionario[] = [];
+  colunas = ['Situacao', 'Nome', 'Sobrenome','Departamento',  'Acoes', 'Excluir']
+
+
+  constructor() {
+
+
+  }
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  openDialog(id: number){
+
+  }
 }
